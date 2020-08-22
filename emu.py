61303e9f8bfc0e0a480c8ffd656e59a8c8569230
steve0320 <srbertol@mtu.edu>
2020-08-22 00:23:23 -0700

    Finish response object definitions Add some convenience methods

--- emu.py
+++ emu.py
@@ -6,7 +6,7 @@ import response_entities
 
 class Emu:
 
-    def __init__(self, debug=False):
+    def __init__(self, debug=False, history_length=10):
 
         # Internal communication
         self._channel_open = False
@@ -19,46 +19,49 @@ class Emu:
         # Data, updated asynchronously by thread, keyed
         # by root element. These are defined by classes
         # in response_entities.py
-        self.data = {}
+        self._data = {}
+
+        # TODO: History, holds last history_length responses.
+        self.history_length = history_length
+        # self.history = {}
+
+    # Get the latest response for the given type. Also marks
+    # the object as stale.
+    def get_response(self, command):
+        e = self._data.get(command)
+        if e is None:
+            return None
+        else:
+            e.fresh = False
+            return e
 
     # Open communication channel
     def start_serial(self, port_name):
 
         if self._channel_open:
-            return
+            return True
 
-        self._serial_port = self._create_serial(port_name)
+        try:
+            self._serial_port = self._create_serial(port_name)
+        except serial.serialutil.SerialException:
+            return False
+
         self._thread_handle = threading.Thread(target=self._communication_thread)
         self._thread_handle.start()
         self._channel_open = True
+        return True
 
     # Close the communication channel
     def stop_serial(self):
 
         if not self._channel_open:
-            return
+            return True
 
         self._stop_thread = True
         self._thread_handle.join()
         self._thread_handle = None
         self._serial_port.close()
         self._serial_port = None
-
-    # Issue a command to the device. Pass the command name as the first
-    # argument, and any additional params as a dict.
-    def issue_command(self, command, params=None):
-
-        root = ElementTree.Element('Command')
-        name_field = ElementTree.SubElement(root, 'Name')
-        name_field.text = command
-
-        if params is not None:
-            for k, v in params.items():
-                field = ElementTree.SubElement(root, k)
-                field.text = v
-
-        bin_string = ElementTree.tostring(root)
-        self._serial_port.write(bin_string)
         return True
 
     # Internal helper for opening serial channel to device
@@ -83,7 +86,8 @@ class Emu:
                 try:
                     tree = ElementTree.fromstringlist(bin_lines)
                 except ElementTree.ParseError:
-                    print("Malformed XML " + bin_lines.decode('UTF-8'))
+                    if self.debug:
+                        print("Malformed XML " + b''.join(bin_lines).decode('ASCII'))
                     continue
 
                 if self.debug:
@@ -92,6 +96,25 @@ class Emu:
                 response_type = tree.tag
                 klass = response_entities.Entity.tag_to_class(response_type)
                 if klass is None:
-                    print("Unsupported tag " + response_type)
+                    if self.debug:
+                        print("Unsupported tag " + response_type)
+                    continue
                 else:
-                    self.data[response_type] = klass(tree)
+                    self._data[response_type] = klass(tree)
+
+    # Issue a command to the device. Pass the command name as the first
+    # argument, and any additional params as a dict.
+    def issue_command(self, command, params=None):
+
+        root = ElementTree.Element('Command')
+        name_field = ElementTree.SubElement(root, 'Name')
+        name_field.text = command
+
+        if params is not None:
+            for k, v in params.items():
+                field = ElementTree.SubElement(root, k)
+                field.text = v
+
+        bin_string = ElementTree.tostring(root)
+        self._serial_port.write(bin_string)
+        return True
--- response_entities.py
+++ response_entities.py
@@ -6,11 +6,17 @@ from xml.etree import ElementTree
 class Entity:
 
     def __init__(self, tree):
+
         self._tree = tree
         self.fresh = True
+
+        # These tags are common to all responses
+        self.device_mac = self.find_text('DeviceMacId')
+
         self._parse()
 
     def __repr__(self):
+        return ElementTree.tostring(self._tree).decode('ASCII')
         return ElementTree.dump(self._tree)
 
     # Hook for subclasses to override to provide special parsing
@@ -21,67 +27,196 @@ class Entity:
     def find_text(self, tag):
         return self._tree.find(tag).text
 
+    # The root element associated with this class
+    @classmethod
+    def tag_name(cls):
+        return cls.__name__
+
     # Map the tag name to the type of subclass
-    @staticmethod
-    def tag_to_class(text):
-        mappings = {
-            'InstantaneousDemand': InstantaneousDemand
-        }
-        return mappings.get(text)
+    @classmethod
+    def tag_to_class(cls, tag):
+        for klass in cls.__subclasses__():
+            if klass.tag_name() == tag:
+                return klass
+        return None
 
 
-class InstantaneousDemand(Entity):
+#####################################
+#       Raven Notifications         #
+#####################################
+
+class ConnectionStatus(Entity):
     def _parse(self):
-        self.demand = self.find_text("Demand")
-        self.multiplier = self.find_text("Multiplier")
-        self.divisor = self.find_text("Divisor")
-
-
-class CurrentSummationDelivered(Entity):
-    def _parse(self):
-        return
-
-
-class CurrentPrice(Entity):
-    def _parse(self):
-        return
+        self.meter_mac = self.find_text('MeterMacId')
+        self.status = self.find_text("Status")
+        self.description = self.find_text("Description")
+        self.status_code = self.find_text("StatusCode")         # 0x00 to 0xFF
+        self.extended_pan_id = self.find_text("ExtPanId")
+        self.channel = self.find_text("Channel")                # 11 to 26
+        self.short_address = self.find_text("ShortAddr")        # 0x0000 to 0xFFFF
+        self.link_strength = self.find_text("LinkStrength")     # 0x00 to 0x64
 
 
 class DeviceInfo(Entity):
     def _parse(self):
-        return
-
-
-class ConnectionStatus(Entity):
-    def _parse(self):
-        return
-
-
-class MessageCluster(Entity):
-    def _parse(self):
-        return
-
-
-class TimeCluster(Entity):
-    def _parse(self):
-        return
-
-
-class NetworkInfo(Entity):
-    def _parse(self):
-        return
-
-
-class PriceCluster(Entity):
-    def _parse(self):
-        return
+        self.install_code = self.find_text("InstallCode")
+        self.link_key = self.find_text("LinkKey")
+        self.fw_version = self.find_text("FWVersion")
+        self.hw_version = self.find_text("HWVersion")
+        self.fw_image_type = self.find_text("ImageType")
+        self.manufacturer = self.find_text("Manufacturer")
+        self.model_id = self.find_text("ModelId")
+        self.date_code = self.find_text("DateCode")
 
 
 class ScheduleInfo(Entity):
     def _parse(self):
-        return
+        self.meter_mac = self.find_text("MeterMacId")
+        self.event = self.find_text("Event")
+        self.frequency = self.find_text("Frequency")
+        self.enabled = self.find_text("Enabled")
 
 
-class BlockPriceDetail(Entity):
+# TODO: There can be more than one MeterMacId
+class MeterList(Entity):
     def _parse(self):
-        return
+        self.meter_mac = self.find_text("MeterMacId")
+
+
+#####################################
+#       Meter Notifications         #
+#####################################
+
+class MeterInfo(Entity):
+    def _parse(self):
+        self.meter_mac = self.find_text("MeterMacId")
+        self.meter_type = self.find_text("MeterType")
+        self.nickname = self.find_text("NickName")
+        self.account = self.find_text("Account")
+        self.auth = self.find_text("Auth")
+        self.host = self.find_text("Host")
+        self.enabled = self.find_text("Enabled")
+
+
+class NetworkInfo(Entity):
+    def _parse(self):
+        self.coordinator_mac = self.find_text("CoordMacId")
+        self.status = self.find_text("Status")
+        self.description = self.find_text("Description")
+        self.status_code = self.find_text("StatusCode")
+        self.extended_pan_id = self.find_text("ExtPanId")
+        self.channel = self.find_text("Channel")
+        self.short_address = self.find_text("ShortAddr")
+        self.link_strength = self.find_text("LinkStrength")
+
+
+#####################################
+#        Time Notifications         #
+#####################################
+
+# TODO: Convert from Rainforest epoch
+class TimeCluster(Entity):
+    def _parse(self):
+        self.meter_mac = self.find_text("MeterMacId")
+        self.utc_time = self.find_text("UTCTime")
+        self.local_time = self.find_text("LocalTime")
+
+
+#####################################
+#      Message Notifications        #
+#####################################
+
+class MessageCluster(Entity):
+    def _parse(self):
+        self.meter_mac = self.find_text("MeterMacId")
+        self.timestamp = self.find_text("TimeStamp")
+        self.id = self.find_text("Id")
+        self.text = self.find_text("Text")
+        self.confirmation_required = self.find_text("ConfirmationRequired")
+        self.confirmed = self.find_text("Confirmed")
+        self.queue = self.find_text("Queue")
+
+
+#####################################
+#        Price Notifications        #
+#####################################
+
+class PriceCluster(Entity):
+    def _parse(self):
+        self.meter_mac = self.find_text("MeterMacId")
+        self.timestamp = self.find_text("TimeStamp")
+        self.price = self.find_text("Price")
+        self.currency = self.find_text("Currency")      # ISO-4217
+        self.trailing_digits = self.find_text("TrailingDigits")
+        self.tier = self.find_text("Tier")
+        self.tier_label = self.find_text("TierLabel")
+        self.rate_label = self.find_text("RateLabel")
+
+
+#####################################
+#   Simple Metering Notifications   #
+#####################################
+
+class InstantaneousDemand(Entity):
+    def _parse(self):
+        self.meter_mac = self.find_text("MeterMacId")
+        self.timestamp = self.find_text("TimeStamp")
+        self.demand = int(self.find_text("Demand"), 16)
+        self.multiplier = int(self.find_text("Multiplier"), 16)
+        self.divisor = int(self.find_text("Divisor"), 16)
+        self.digits_right = self.find_text("DigitsRight")
+        self.digits_left = self.find_text("DigitsLeft")
+        self.suppress_leading_zero = self.find_text("SuppressLeadingZero")
+
+        # Compute actual reading
+        self.reading = self.demand * self.multiplier / float(self.divisor)
+
+
+class CurrentSummationDelivered(Entity):
+    def _parse(self):
+        self.meter_mac = self.find_text("MeterMacId")
+        self.timestamp = self.find_text("TimeStamp")
+        self.summation_delivered = self.find_text("SummationDelivered")
+        self.summation_received = self.find_text("SummationReceived")
+        self.multiplier = self.find_text("Multiplier")
+        self.divisor = self.find_text("Divisor")
+        self.digits_right = self.find_text("DigitsRight")
+        self.digits_left = self.find_text("DigitsLeft")
+        self.suppress_leading_zero = self.find_text("SuppressLeadingZero")
+
+
+class CurrentPeriodUsage(Entity):
+    def _parse(self):
+        self.meter_mac = self.find_text("MeterMacId")
+        self.timestamp = self.find_text("TimeStamp")
+        self.current_usage = self.find_text("CurrentUsage")
+        self.multiplier = self.find_text("Multiplier")
+        self.divisor = self.find_text("Divisor")
+        self.digits_right = self.find_text("DigitsRight")
+        self.digits_left = self.find_text("DigitsLeft")
+        self.suppress_leading_zero = self.find_text("SuppressLeadingZero")
+        self.start_date = self.find_text("StartDate")
+
+
+class LastPeriodUsage(Entity):
+    def _parse(self):
+        self.meter_mac = self.find_text("MeterMacId")
+        self.last_usage = self.find_text("LastUsage")
+        self.multiplier = self.find_text("Multiplier")
+        self.divisor = self.find_text("Divisor")
+        self.digits_right = self.find_text("DigitsRight")
+        self.digits_left = self.find_text("DigitsLeft")
+        self.suppress_leading_zero = self.find_text("SuppressLeadingZero")
+        self.start_date = self.find_text("StartDate")
+        self.end_date = self.find_text("EndDate")
+
+
+# TODO: IntervalData may appear more than once
+class ProfileData(Entity):
+    def _parse(self):
+        self.meter_mac = self.find_text("MeterMacId")
+        self.end_time = self.find_text("EndTime")
+        self.status = self.find_text("Status")
+        self.period_interval = self.find_text("ProfileIntervalPeriod")
+        self.number_of_periods = self.find_text("NumberOfPeriodsDelivered")
+        self.interval_data = self.find_text("IntervalData")
