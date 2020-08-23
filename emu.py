968b4182c17a0af59ad9deb0c3eaf8800cee831d
steve0320 <srbertol@mtu.edu>
2020-08-22 17:54:40 -0700

    Add synchronous mode

--- emu.py
+++ emu.py
@@ -1,12 +1,13 @@
 import serial
 import threading
 from xml.etree import ElementTree
+import time
 import response_entities
 
 
 class Emu:
 
-    def __init__(self, debug=False):
+    def __init__(self, debug=False, synchronous=False):
 
         # Internal communication
         self._channel_open = False
@@ -15,6 +16,7 @@ class Emu:
         self._stop_thread = False
 
         self.debug = debug
+        self.synchronous = synchronous
 
         # Data, updated asynchronously by thread, keyed
         # by root element. These are defined by classes
@@ -22,16 +24,6 @@ class Emu:
         self._data = {}
 
         # TODO: Implement history mechanism
-
-    # Get the latest response for the given type. Also marks
-    # the object as stale.
-    def get_response(self, command):
-        e = self._data.get(command)
-        if e is None:
-            return None
-        else:
-            e.fresh = False
-            return e
 
     # Open communication channel
     def start_serial(self, port_name):
@@ -102,8 +94,12 @@ class Emu:
                     self._data[response_type] = klass(tree)
 
     # Issue a command to the device. Pass the command name as the first
-    # argument, and any additional params as a dict.
-    def issue_command(self, command, params=None):
+    # argument, and any additional params as a dict. Will return immediately
+    # unless the synchronous attribute on the library is true, in which case
+    # it will return data when available, or None if the timeout has elapsed.
+    def issue_command(self, command, params=None, return_class=None, timeout=5, poll_factor=2):
+
+        # TODO: Ensure stream open
 
         root = ElementTree.Element('Command')
         name_field = ElementTree.SubElement(root, 'Name')
@@ -116,8 +112,32 @@ class Emu:
                     field.text = v
 
         bin_string = ElementTree.tostring(root)
+
+        if (not self.synchronous) or return_class is None:
+            if self.debug:
+                print("Object is in asynchronous mode or command does not have return type - not waiting for response")
+            self._serial_port.write(bin_string)
+            return True
+
+        # Do our best to return results synchronously
+        tag = return_class.tag_name()
+
+        # Invalidate current response
+        cur = self._data.get(tag)
+        if cur is not None:
+            cur.fresh = False
+
         self._serial_port.write(bin_string)
-        return True
+
+        step = 1.0 / poll_factor
+        for i in range(0, timeout * poll_factor):
+            d = self._data.get(tag)
+            if d is not None and d.fresh:
+                return d
+            else:
+                time.sleep(step)
+
+        return None
 
     # Convert boolean to Y/N for commands
     def _format_yn(self, value):
@@ -139,14 +159,18 @@ class Emu:
     #         Raven Commands        #
     #################################
 
+    def restart(self):
+        return self.issue_command('restart')
+
+    # Dangerous! Will decommission device!
     def factory_reset(self):
-        self.issue_command('factory_reset')
+        return self.issue_command('factory_reset')
 
     def get_connection_status(self):
-        self.issue_command('get_connection_status')
+        return self.issue_command('get_connection_status', return_class=response_entities.ConnectionStatus)
 
     def get_device_info(self):
-        self.issue_command('get_device_info')
+        return self.issue_command('get_device_info', return_class=response_entities.DeviceInfo)
 
     def get_schedule(self, mac=None, event=None):
 
@@ -154,7 +178,7 @@ class Emu:
             raise ValueError('Valid events are time, price, demand, summation, or message')
 
         opts = {'MeterMacId': mac, 'Event': event}
-        self.issue_command('get_schedule', opts)
+        return self.issue_command('get_schedule', opts, return_class=response_entities.ScheduleInfo)
 
     def set_schedule(self, mac=None, event=None, frequency=10, enabled=True):
 
@@ -167,7 +191,7 @@ class Emu:
             'Frequency': self._format_hex(frequency),
             'Enabled': self._format_yn(enabled)
         }
-        self.issue_command('set_schedule', opts)
+        return self.issue_command('set_schedule', opts)
 
     def set_schedule_default(self, mac=None, event=None):
 
@@ -175,10 +199,10 @@ class Emu:
             raise ValueError('Valid events are time, price, demand, summation, or message')
 
         opts = {'MeterMacId': mac, 'Event': event}
-        self.issue_command('set_schedule_default', opts)
+        return self.issue_command('set_schedule_default', opts)
 
     def get_meter_list(self):
-        self.issue_command('get_meter_list')
+        return self.issue_command('get_meter_list', return_class=response_entities.MeterList)
 
     ##########################
     #     Meter Commands     #
@@ -186,10 +210,10 @@ class Emu:
 
     def get_meter_info(self, mac=None):
         opts = {'MeterMacId': mac}
-        self.issue_command('get_meter_info', opts)
+        return self.issue_command('get_meter_info', opts, return_class=response_entities.MeterInfo)
 
     def get_network_info(self):
-        self.issue_command('get_network_info')
+        return self.issue_command('get_network_info', return_class=response_entities.NetworkInfo)
 
     def set_meter_info(self, mac=None, nickname=None, account=None, auth=None, host=None, enabled=None):
 
@@ -201,7 +225,7 @@ class Emu:
             'Host': host,
             'Enabled': self._format_yn(enabled)
         }
-        self.issue_command('set_meter_info', opts)
+        return self.issue_command('set_meter_info', opts)
 
     ############################
     #       Time Commands      #
@@ -209,11 +233,11 @@ class Emu:
 
     def get_time(self, mac=None, refresh=True):
         opts = {'MeterMacId': mac, 'Refresh': self._format_yn(refresh)}
-        self.issue_command('get_time', opts)
+        return self.issue_command('get_time', opts, return_class=response_entities.TimeCluster)
 
     def get_message(self, mac=None, refresh=True):
         opts = {'MeterMacId': mac, 'Refresh': self._format_yn(refresh)}
-        self.issue_command('get_message', opts)
+        return self.issue_command('get_message', opts, return_class=response_entities.MessageCluster)
 
     def confirm_message(self, mac=None, message_id=None):
 
@@ -221,7 +245,7 @@ class Emu:
             raise ValueError('Message id is required')
 
         opts = {'MeterMacId': mac, 'Id': self._format_hex(message_id)}
-        self.issue_command('confirm_message', opts)
+        return self.issue_command('confirm_message', opts)
 
     #########################
     #     Price Commands    #
@@ -229,7 +253,7 @@ class Emu:
 
     def get_current_price(self, mac=None, refresh=True):
         opts = {'MeterMacId': mac, 'Refresh': self._format_yn(refresh)}
-        self.issue_command('get_current_price', opts)
+        return self.issue_command('get_current_price', opts, return_class=response_entities.PriceCluster)
 
     # Price is in cents, w/ decimals (e.g. "24.373")
     def set_current_price(self, mac=None, price="0.0"):
@@ -247,7 +271,7 @@ class Emu:
             'Price': self._format_hex(price),
             'TrailingDigits': self._format_hex(trailing, digits=2)
         }
-        self.issue_command('set_current_price', opts)
+        return self.issue_command('set_current_price', opts)
 
     ###############################
     #   Simple Metering Commands  #
@@ -255,23 +279,23 @@ class Emu:
 
     def get_instantaneous_demand(self, mac=None, refresh=True):
         opts = {'MeterMacId': mac, 'Refresh': self._format_yn(refresh)}
-        self.issue_command('get_instantaneous_demand', opts)
+        return self.issue_command('get_instantaneous_demand', opts, return_class=response_entities.InstantaneousDemand)
 
     def get_current_summation_delivered(self, mac=None, refresh=True):
         opts = {'MeterMacId': mac, 'Refresh': self._format_yn(refresh)}
-        self.issue_command('get_current_summation_delivered', opts)
+        return self.issue_command('get_current_summation_delivered', opts, return_class=response_entities.CurrentSummationDelivered)
 
     def get_current_period_usage(self, mac=None):
         opts = {'MeterMacId': mac}
-        self.issue_command('get_current_period_usage', opts)
+        return self.issue_command('get_current_period_usage', opts, return_class=response_entities.CurrentPeriodUsage)
 
     def get_last_period_usage(self, mac=None):
         opts = {'MeterMacId': mac}
-        self.issue_command('get_last_period_usage', opts)
+        return self.issue_command('get_last_period_usage', opts, return_class=response_entities.LastPeriodUsage)
 
     def close_current_period(self, mac=None):
         opts = {'MeterMacId': mac}
-        self.issue_command('close_current_period', opts)
+        return self.issue_command('close_current_period', opts)
 
     def set_fast_poll(self, mac=None, frequency=4, duration=20):
         opts = {
@@ -279,4 +303,4 @@ class Emu:
             'Frequency': self._format_hex(frequency, digits=4),
             'Duration': self._format_hex(duration, digits=4)
         }
-        self.issue_command('set_fast_poll', opts)
+        return self.issue_command('set_fast_poll', opts)
